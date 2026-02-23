ee4494b90430152575ffdc12ac0dd83c25521b1e
1922590 <1922590@01-18104.pusd.dom>
2026-02-23 11:33:11 -0800

    update - map prints out and data is secured in 2D array

--- Q3 Project 1 Period 2/src/mapRunner.java
+++ Q3 Project 1 Period 2/src/mapRunner.java
@@ -5,11 +5,36 @@ import java.util.Scanner;
 public class mapRunner {
 
 	public static void main(String[] args) {
-		File map = new File("EZmap1");
+		File map = new File("MIDmap1");
 		try {
 			Scanner mapScan = new Scanner(map);
-			while(mapScan.hasNext()) {
-				System.out.println(mapScan.next());
+			
+			int rowNum = Integer.parseInt(mapScan.next());
+			int colNum = Integer.parseInt(mapScan.next());
+			int levelNum = Integer.parseInt(mapScan.next());
+			//mapLay contains the values of all elements in the map accurately
+			String[][] mapLay = new String[rowNum][colNum];
+			for (int r = 0; r < mapLay.length; r++) {
+				
+				for (int c = 0; c < mapLay[r].length; c++) {
+					String temp = mapScan.next();
+					mapLay[r][c] = temp;
+				}
+				
+			}
+			//prints the bases
+			System.out.println(rowNum);
+			System.out.println(colNum);
+			System.out.println(levelNum);
+			//prints out the map in its entirety
+			for (int r = 0; r < mapLay.length; r++) {
+				
+				String res = "";
+				for (int c = 0; c < mapLay[r].length; c++) {
+					res += mapLay[r][c] + " ";
+				}
+				System.out.println(res);
+				System.out.println("\n");
 			}
 			
 			
@@ -20,5 +45,6 @@ public class mapRunner {
 		}
 
 	}
+
 
 }
