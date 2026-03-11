41d51ac4713fac5a28c09719e18fbee5cce5e40e
1922590 <1922590@01-18104.pusd.dom>
2026-03-11 11:45:17 -0700

    update - added Coord class

---
+++ Q3 Project 1 Period 2/src/Coord.java
@@ -0,0 +1,31 @@
+
+public class Coord {
+   private int row;
+   private int col;
+   private int lev;
+   public Coord(int row, int col, int lev) {
+       this.row = row;
+       this.col = col;
+       this.lev = lev;
+   }
+   public Coord() {}
+   public int getRow() {
+   	return row;
+   }
+   public int getCol() {
+   	return col;
+   }
+   public int getLev() {
+   	return lev;
+   }
+   public void setRow(int r) {
+   	this.row = r;
+   }
+   public void setCol(int c) {
+   	this.col = c;
+   }
+   public void setLev(int l) {
+   	this.lev = l;
+   }
+}
+
