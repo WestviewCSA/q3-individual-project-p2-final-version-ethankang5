7d3e1d8048fd510daa5d97d4c57e0bd1c432e773
1922590 <1922590@01-18104.pusd.dom>
2026-03-11 11:45:59 -0700

    update - stackTraversal

--- Q3 Project 1 Period 2/src/mapRunner.java
+++ Q3 Project 1 Period 2/src/mapRunner.java
@@ -1,11 +1,6 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
-
+import java.util.Stack;
 public class mapRunner {
-
 	
 	
 	
@@ -30,36 +25,71 @@ public class mapRunner {
 	
 	public static void main(String[] args) {
 		File map = new File("HARDmap4");
-		File coor = new File("MIDmapCOOR");
-	
+		File coor = new File("HARDmapCOOR");
+		
+		
 		String[][][] myNormMap = normalMapper.normMap(map);
-	    if (myNormMap != null && normalMapper.isWolverine(myNormMap)) {
-	        printer(myNormMap);
-	    } else {
-	        System.out.println("There is no Wolverine!");
-	    }
-
 	    String[][][] myCoorMap = coordinateMapper.coorMap(coor);
-	    if (myCoorMap != null && coordinateMapper.isWolverine(myCoorMap)) {
-	        printer(myCoorMap);
-	    } else {
-	        System.out.println("There is no Wolverine!");
+	    stackTraverse(myNormMap);
+	    printer(myNormMap);
+			
+			
+		
+		
+		
+	}
+	
+	public static void stackTraverse(String[][][] map) {
+	    Stack<Coord> stacker = new Stack<Coord>();
+	    boolean[][][] visited = new boolean[map.length][map[0].length][map[0][0].length];
+	    if (!mapHelpers.isWolverine(map)) {
+	        return;
 	    }
-		
-		
-		
-
+	    Coord start = mapHelpers.wolverineHop(map, 0);
+	    stacker.push(start);
+	    while (!stacker.empty()) {
+	        Coord current = stacker.pop();
+	        int l = current.getLev();
+	        int r = current.getRow();
+	        int c = current.getCol();
+	        if (l < 0 || l >= map.length || r < 0 || r >= map[l].length || c < 0 || c >= map[l][r].length) {
+	            continue;
+	        }
+	        if (visited[l][r][c]) {
+	            continue;
+	        }
+	        visited[l][r][c] = true;
+	        if (map[l][r][c].equals("$")) {
+	            System.out.println("Goal found!");
+	            return;
+	        } 
+	        if (map[l][r][c].equals(".") || map[l][r][c].equals("|")) {
+	        	//check for jump gate
+	            if (!map[l][r][c].equals("|")) map[l][r][c] = "+";
+	        }
+	        boolean[] moveKey = mapHelpers.canMove(map, current);
+	        // North
+	        if (moveKey[0]) stacker.push(new Coord(r - 1, c, l));
+	        // South
+	        if (moveKey[1]) stacker.push(new Coord(r + 1, c, l));
+	        // East
+	        if (moveKey[2]) stacker.push(new Coord(r, c + 1, l));
+	        // West
+	        if (moveKey[3]) stacker.push(new Coord(r, c - 1, l));
+	       
+	        if (moveKey[4]) {
+	            Coord nextLevelStart = mapHelpers.wolverineHop(map, l + 1);
+	            if (nextLevelStart != null) {
+	                stacker.push(nextLevelStart);
+	            }
+	        }
+	    }
 	}
 	
-	public void stackTraverse(HashMap<String, Integer> key, String[][][] map) {
-		
-	}
-	
-	public void queueTraverse(HashMap<String, Integer> key, String[][][] map) {
-		
+	public static void queueTraverse(String[][][] map) {
+	   
 	}
 	
 	
-
-
 }
+
