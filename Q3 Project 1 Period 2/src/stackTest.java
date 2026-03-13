a8f20198fdd45711e57dd2d48c4162b89679725a
1922590 <1922590@01-18104.pusd.dom>
2026-03-13 11:11:13 -0700

    add - JUnit test for stack imp.

---
+++ Q3 Project 1 Period 2/src/stackTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+class stackTest {
+	@Test
+   public void testPath() {
+       String[][][] testMap = { { {"W", ".", "."}, {"@", ".", "@"}, {"$", ".", "."} } };
+       mapRunner.stackTraverse(testMap);
+       //check if traversal makes it around the wall
+       assertEquals("+", testMap[0][0][1], "Tile (0,1) should be marked as visited");
+       assertEquals("+", testMap[0][1][1], "Tile (1,1) should be marked as visited");
+      
+       //Wolverine and buck should NOT be changed
+       assertEquals("$", testMap[0][2][0], "Buck symbol should remain '$'");
+       assertEquals("W", testMap[0][0][0], "Wolv symbol should remain 'W'");
+   }
+   @Test
+   public void testImpossiblePath() {
+       String[][][] blockedMap = { { {"W", ".", "@"}, {"@", "@", "@"}, {"$", ".", "."} } };
+       mapRunner.stackTraverse(blockedMap);
+       assertEquals(".", blockedMap[0][2][1], "Blocked area should remain '.'");
+   }
+   @Test
+   public void testMultiLevelJump() {
+   	//map has 2 levels
+       String[][][] levelMap = { {  {"W", "|"} }, {  {"W", "$"} } };
+       mapRunner.stackTraverse(levelMap);
+       //check if properly uses portal
+       assertEquals("$", levelMap[1][0][1], "Goal on second level index 1 should be found");
+   }
+}
